dce67ceb56671d72f6e951f9fd11205218d52c2d
Arefin <rafin.ryan.07@outlook.com>
2018-01-28 22:01:58 +0600

    heroku port updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -40,6 +41,6 @@ app.get('/about', (req, res) => {
     res.render('about.hbs');
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000!");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
